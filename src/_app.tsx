545ee1501d07e8de48fb1e858ed9e3fe81acd9d6
Ivan Duran <iamivanduran02@gmail.com>
2022-11-13 11:19:46 -0500

    final touches

--- src/_app.tsx
+++ src/_app.tsx
@@ -1,17 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { TRPCProvider } from "./utils/trpc";
+// import { TRPCProvider } from "./utils/trpc";
 
-import { HomeScreen } from "./screens/home";
+import HomeScreen from "./screens/home";
 
+// uncomment if using tRPC
 export const App = () => {
   return (
-    <TRPCProvider>
-      <SafeAreaProvider>
-        <HomeScreen />
-        <StatusBar />
-      </SafeAreaProvider>
-    </TRPCProvider>
+    // <TRPCProvider>
+    <SafeAreaProvider>
+      <HomeScreen />
+      <StatusBar />
+    </SafeAreaProvider>
+    // </TRPCProvider>
   );
 };
